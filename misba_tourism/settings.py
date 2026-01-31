89fc30ce8521a81c9ab4195c3afc480d1119ddc1
Athithyan07 <athithyans07@gmail.com>
2026-01-31 16:06:48 +0530

    Update: Set new Google App Password as fallback in settings

--- misba_tourism/settings.py
+++ misba_tourism/settings.py
@@ -99,9 +99,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'misbatourismkkl@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'wart bkel vqkh enlf')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER', 'misbatourismkkl@gmail.com')
 ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL', 'misbatourismkkl@gmail.com')
 
 CORS_ALLOW_ALL_ORIGINS = True
